120a2e6d068230b17fc4530174ce5dc143ad125b
SolCharms <ckioulos@uottawa.ca>
2023-10-01 13:48:46 -0400

    code clean-up

--- src/cli_configs/devnet/networkConfig.ts
+++ src/cli_configs/devnet/networkConfig.ts
@@ -6,8 +6,8 @@ type NetworkConfig = {
 export const networkConfig: NetworkConfig =
     {
         clusterApiUrl: "https://api.devnet.solana.com",
-        // signerKeypair: "/home/charalambos/.config/solana/devnet-challenger/crux_manager.json" // Crux Manager Account
-        signerKeypair: "/home/charalambos/.config/solana/devnet-challenger/moderator_1.json" // Moderator 1 Account
+        signerKeypair: "/home/charalambos/.config/solana/devnet-challenger/crux_manager.json" // Crux Manager Account
+        // signerKeypair: "/home/charalambos/.config/solana/devnet-challenger/moderator_1.json" // Moderator 1 Account
         // signerKeypair: "/home/charalambos/.config/solana/devnet-challenger/user_1.json" // User 1 Account
         // signerKeypair: "/home/charalambos/.config/solana/devnet-test1.json" // Deploy Keypair
     }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { PublicKey } from '@solana/web3.js';
 
-// export * from './challenger.client';
-// export * from './challenger.pda';
-
 export const CHALLENGER_PROG_ID = new PublicKey(
     'CRuXQ86F4m6VfRHa7VACNbQKJoSioG3gcpui9BH2YNWa'
 );
